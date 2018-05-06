3c5f7eb50eb5795fbb645e73fa6c29d7edbc3dc2
G-sharp <123@bupt.edu.cn>
2018-05-07 00:06:45 +0800

    修改：     8queen.py

--- exercises/8queen.py
+++ exercises/8queen.py
@@ -7,21 +7,21 @@ Created on Sat May  5 21:57:08 2018
 """
 #* queen problem with recurison
 BOARD_SIZE = 8
- 
+
 def under_attack(col, queens):
    left = right = col
    for r, c in reversed(queens):
- #左右有冲突的位置的列号      
+ #左右有冲突的位置的列号
        left, right = left - 1, right + 1
- 
+
        if c in (left, col, right):
            return True
    return False
- 
+
 def solve(n):
    if n == 0:
        return [[]]
- 
+
    smaller_solutions = solve(n - 1)
 
    return [solution+[(n,i+1)]
@@ -30,5 +30,4 @@ def solve(n):
                if not under_attack(i+1, solution)]
 for answer in solve(BOARD_SIZE):
    print answer
-    
-        
+   
